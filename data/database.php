d155b818f6275db77afd7bb35bf735e1212cbcf6
Younes80 <57212570+Younes80@users.noreply.github.com>
2022-07-06 10:02:29 +0200

    modification database

--- data/database.php
+++ data/database.php
@@ -1,14 +1,8 @@
 <?php
 require_once './.env';
-// $pdo = new PDO('mysql:host=localhost;dbname=sql_base', 'root', 'ApQm102938475G!!');
-// echo 'connexion ok';
-// return $pdo;
-
-
-
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=sql_base', $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], [
+    $pdo = new PDO('mysql:host=localhost;dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
